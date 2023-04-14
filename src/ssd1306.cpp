4951efbf0342b296892d3272ac43d23a7ed8e488
s4kkkk <nomail@nomail.nomail>
2023-04-14 23:11:48 +0300

    Определена структруа класса ssd1306. Добавлены основные объявления

--- src/ssd1306.cpp
+++ src/ssd1306.cpp
@@ -1,1 +1,25 @@
+#include "../headers/ssd1306.hpp"
 
+#ifndef COMMON_HEADERS
+#define COMMON_HEADERS
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#endif
+
+uint16_t ssd1306::GetMaxLine()
+{
+    return maxline;
+}
+
+uint16_t ssd1306::GetMaxColumn()
+{
+    return maxcolumn;
+}
+
+ssd1306::ssd1306(uint32_t bitrate)
+{
+    //инициализация TWI и ssd1306 модуля
+}
